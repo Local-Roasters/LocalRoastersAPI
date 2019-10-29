202f7ef2d11f8bba01eb9c6be1a0027b4b893235
Mark Goldstein <markgoldstein12@gmail.com>
2019-10-29 15:12:38 -0400

    user model updated with location

--- models/User.js
+++ models/User.js
@@ -1,6 +1,10 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
+  phoneID: {
+    type: Number,
+    required: true
+  },
   location: {
     type: String,
     required: true
